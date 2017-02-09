1a980f5d9685eba7e4bced1d1fcfa60556b0e447
yubo <yubo1911@163.com>
2017-02-09 00:48:27 -0800

    render templates

---
+++ venv/render_template/render.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello_world(name=None):
+    return render_template('hello.html', name=name)
