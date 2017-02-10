696f77c05a770e105b88fccdc0c21842469d60a7
yubo <yubo1911@163.com>
2017-02-10 00:47:35 -0800

    tutorial blog app flaskr

---
+++ flaskr/flaskr/flaskr.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'flaskr.db'),
+    SECRET_KEY='development key',
+    USERNAME='admin',
+    PASSWORD='default'
+))
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
