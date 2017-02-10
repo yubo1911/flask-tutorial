7eabe4540cb2f596a356b7de38860a647300fc0a
yubo <yubo1911@163.com>
2017-02-09 23:18:55 -0800

    flashing

---
+++ flashing/hello.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, request, flash, redirect, url_for
+
+app = Flask(__name__)
+
+app.secret_key = 'secret_key'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+                request.form['password'] != 'secret':
+            error = 'Invalid credentials'
+        else:
+            flash('You were successfully logged in')
+            return redirect(url_for('index'))
+
+    return render_template('login.html', error=error)
